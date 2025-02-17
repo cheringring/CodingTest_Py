d29f26a84989487ec2a7b8ef82cc68751e669782
cheringring <130526634+cheringring@users.noreply.github.com>
2025-02-17 22:17:22 +0900

    Update 자릿수 더하기.py

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -1,8 +1,14 @@
 def solution(n):
     answer = 0
 
+    #정수로 더해야하니까 int(x) 넣어주고
+    #반복문할때 각 자릿씩 뽑기 위해 문자열로 변환시켜주고
+    # for반복문을 통해 각 자릿수 하나씩 가져옴
+    
+    # n을 문자열로 변환 -> 한자릿씩 가져와서 x담음.
+    # x를 정수화함. ->  sum
     answer = sum(int(x) for x in str(n))
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+
     print('Hello Python')
 
     return answer
